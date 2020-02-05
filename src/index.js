ac55bfc4979020e1c57e705fadf2d68a084bddbf
Javier Ocampo <ferroxido@gmail.com>
2020-02-04 22:49:34 -0300

    Setup simple server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+'use strict'
+
+const http = require('http')
+const express = require('express')
 const { isMutant } = require('./services')
 
+const app = express()
+const server = http.createServer(app)
+const port = process.env.PORT || 3000
 const dna = ['3TGCGA', 'CAGTGC', 'TTATGT', 'AGAA2G', '1CCCTA', 'TCACTG']
-const dnaArray = dna.map(e => Array.from(e))
-console.log(dnaArray)
-isMutant(dnaArray).then(res => console.log(`Is mutant: ${res}`))
-console.log('Not blocking event loop')
+
+server.listen(port, () => {
+  console.log(`Server listen on port ${port}`)
+})
+
+// const dnaArray = dna.map(e => Array.from(e))
+// console.log(dnaArray)
+// isMutant(dnaArray).then(res => console.log(`Is mutant: ${res}`))
+// console.log('Not blocking event loop')
